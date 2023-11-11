646f12f2cb4e2d8c4c9728563e1e1e71b593e9b9
Alexey Besedin <cdr800mb@mail.ru>
2023-11-11 23:55:07 +0400

    Add UserController

---
+++ app/src/main/java/hexlet/code/controller/UserController.java
@@ -0,0 +1,51 @@
+package hexlet.code.controller;
+
+import hexlet.code.dto.UserCreateDTO;
+import hexlet.code.dto.UserDTO;
+import hexlet.code.dto.UserUpdateDTO;
+import hexlet.code.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/api/users")
+public class UserController {
+
+    @Autowired
+    private UserService service;
+
+    @GetMapping(path = "")
+    @ResponseStatus(HttpStatus.OK)
+    public List<UserDTO> index() {
+        return service.getAll();
+    }
+
+    @GetMapping(path = "/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public UserDTO show(@PathVariable long id) {
+        return service.findById(id);
+    }
+
+    @PostMapping(path = "")
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserDTO create(@Valid @RequestBody UserCreateDTO userData) {
+        return service.create(userData);
+    }
+
+    @PutMapping(path = "/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public UserDTO update(@Valid @RequestBody UserUpdateDTO userData, @PathVariable long id) {
+        return service.update(userData, id);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable long id) {
+        service.delete(id);
+    }
+
+}
