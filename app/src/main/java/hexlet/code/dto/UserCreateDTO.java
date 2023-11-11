992b576ef486e8e5e38bf115f99a1d89c178c6ac
Alexey Besedin <cdr800mb@mail.ru>
2023-11-11 22:42:41 +0400

    Add UserCreateDTO

---
+++ app/src/main/java/hexlet/code/dto/UserCreateDTO.java
@@ -0,0 +1,25 @@
+package hexlet.code.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserCreateDTO {
+
+    @NotNull
+    @Email(message = "Wrong email format!")
+    private String email;
+
+    private String firstName;
+
+    private String lastName;
+
+    @NotNull
+    @Size(min=3,message = "Minimal password length is about 3 symbols!")
+    private String password;
+
+}
