dda5888aa7ebc9555d51013f457bcbcd8bccc822
Alexey Besedin <cdr800mb@mail.ru>
2023-11-11 23:10:23 +0400

    Add mappers for enity conversion

---
+++ app/src/main/java/hexlet/code/mapper/UserMapper.java
@@ -0,0 +1,19 @@
+package hexlet.code.mapper;
+
+import hexlet.code.dto.UserCreateDTO;
+import hexlet.code.dto.UserDTO;
+import hexlet.code.dto.UserUpdateDTO;
+import hexlet.code.model.User;
+import org.mapstruct.*;
+
+@Mapper(
+        uses =  { JsonNullableMapper.class },
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+        componentModel = MappingConstants.ComponentModel.SPRING,
+        unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
+public abstract class UserMapper {
+    public abstract User map(UserCreateDTO userData);
+    public abstract UserDTO map(User user);
+    public abstract void update(UserUpdateDTO userData, @MappingTarget User user);
+}
