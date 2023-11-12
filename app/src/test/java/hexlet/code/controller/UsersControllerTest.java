af4f78dbf760453c1f70071b4bbedada7de3a805
Alexey Besedin <cdr800mb@mail.ru>
2023-11-12 04:27:32 +0400

    Add UserController test

---
+++ app/src/test/java/hexlet/code/controller/UsersControllerTest.java
@@ -0,0 +1,116 @@
+package hexlet.code.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hexlet.code.mapper.UserMapper;
+import hexlet.code.model.User;
+import hexlet.code.repository.UserRepository;
+import net.datafaker.Faker;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.web.servlet.MockMvc;
+
+import org.springframework.http.MediaType;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UsersControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private UserRepository repository;
+    @Autowired
+    private Faker faker;
+
+    @Autowired
+    private UserMapper mapper;
+
+    @Autowired
+    private PasswordEncoder encoder;
+
+    @Autowired
+    private ObjectMapper om;
+
+    private User testUser;
+
+    @BeforeEach
+    public void setUp() {
+        testUser = new User();
+        testUser.setFirstName(faker.name().firstName());
+        testUser.setLastName(faker.name().lastName());
+        testUser.setEmail(faker.internet().emailAddress());
+        testUser.setPassword(encoder.encode(faker.internet().password()));
+        repository.save(testUser);
+    }
+
+    @Test
+    public void testIndex() throws Exception {
+        mockMvc.perform(get("/api/users"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testShow() throws Exception {
+        mockMvc.perform(get("/api/users/{id}", testUser.getId()))
+                .andExpect(status().isOk())
+                .andExpect(content().json(om.writeValueAsString(mapper.map(testUser))));
+    }
+
+    @Test
+    public void testCreate() throws Exception{
+        User user = new User();
+        user.setFirstName(faker.name().firstName());
+        user.setLastName(faker.name().lastName());
+        user.setEmail(faker.internet().emailAddress());
+        user.setPassword(faker.internet().emailAddress());
+
+        MockHttpServletRequestBuilder request = post("/api/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(om.writeValueAsString(user));
+        mockMvc.perform(request).andExpect(status().isCreated());
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+        User user = new User();
+        user.setFirstName(faker.name().firstName());
+        user.setLastName(faker.name().lastName());
+
+        MockHttpServletRequestBuilder request = put("/api/users/{id}", testUser.getId())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(om.writeValueAsString(user));
+
+        mockMvc.perform(request)
+                .andExpect(status().isOk());
+
+        User actualUser = repository.findById(testUser.getId()).get();
+
+        assertThat(actualUser.getFirstName()).isEqualTo(user.getFirstName());
+        assertThat(actualUser.getLastName()).isEqualTo(user.getLastName());
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+
+        mockMvc.perform(delete("/api/users/{id}", testUser.getId()))
+                .andExpect(status().isNoContent());
+        assertThat(repository.findById(testUser.getId())).isEmpty();
+    }
+
+
+}
